8b7de0b7ea8279e860752506c0dbb63bcad615f0
Alex Orellana <lexorel88@gmail.com>
2024-10-28 11:29:50 -0400

    Initial commit

---
+++ src/main/java/com/historiaclinica/historiaclinica/HistoriaclinicaApplication.java
@@ -0,0 +1,13 @@
+package com.historiaclinica.historiaclinica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoriaclinicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoriaclinicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/historiaclinica/historiaclinica/HistoriaclinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.historiaclinica.historiaclinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoriaclinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
